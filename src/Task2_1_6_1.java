4f870433f7409f8db965de062ced14df61fbbc85
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-06-26 12:44:21 +0300

    Initial commit

--- src/Task2_1_6_1.java
+++ src/Task2_1_6_1.java
@@ -1,5 +1,6 @@
-public class Main {
+public class Task2_1_6_1 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание : \n1.\tУрок 6. Задание «Обработка фото с помощью лямбд» ч 3\n\nРешение: ");
+        System.out.println("Ознакомлен");
     }
 }
